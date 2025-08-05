54ef83749beb9c4ae488091e3710056226d2ac65
Nunners <juliannachua@icloud.com>
2025-08-05 11:58:01 -0700

    Added a few more characters, small fixes

--- script.js
+++ script.js
@@ -6,12 +6,18 @@ const characters = [
     */
     {name: "Cecilia", desc: "otori dessssuuuu"},
     {name: "Cistern", desc: "totally tubular"},
+    {name: "Cristy", desc: "why so serious"},
+    {name: "Dista", desc: "YAYAYAYAYAY"},
+    {name: "Endo", desc: "stay away from me"},
+    {name: "Gigi", desc: "lil shy boi"},
 ]
 
 characters.forEach(function(i){
     let icon = document.createElement("div");
     icon.classList.add("char");
+    icon.title = i.desc;
     let name = document.createElement("span");
+    name.classList.add("name");
     name.innerHTML = i.name;
     icon.appendChild(name);
     chars.appendChild(icon);
